be61511fcaacc00b92566ec14027f01c29d8db5b
ElieBadari <elie.badari@hotmail.com>
2022-02-16 21:20:49 +0200

    added game components

--- app/src/main/java/com/example/tictactoe_game/MainActivity.java
+++ app/src/main/java/com/example/tictactoe_game/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tictactoe_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void tic(View view){
+        //Toast.makeText(this,"I'm here",Toast.LENGTH_LONG).show(); testing if the onclick for all images is working
+        ImageView tic = (ImageView) view;
+        tic.setImageResource(R.drawable.yellow);
+        tic.setTranslationY(-1500);
+        tic.animate().translationYBy(1500).setDuration(600);
+
+    }
+
 }
